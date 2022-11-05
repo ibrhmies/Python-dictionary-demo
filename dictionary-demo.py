e1eb83c9d860ea5376665e590f748249ae2d9b1a
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-11-05 23:32:33 +0300

    dictionary-demo
    Python'da dictionary uygulamaları

---
+++ dictionary-demo.py
@@ -0,0 +1,21 @@
+#1-> ürün bilgisini (id,ad,fiyat) kullanıcıdan aldığınız bilgilerle dictionary içinde saklayınız.
+
+id = input("Ürünün id bilgisini girin:")
+
+ad = input("Ürünün ismini giriniz:")
+
+fiyat = input("Ürünün fiyatını giriniz:")
+
+
+urun = {"id":id, "ad":ad, "fiyat":fiyat}
+
+print(urun)
+
+
+
+
+
+
+
+
+
